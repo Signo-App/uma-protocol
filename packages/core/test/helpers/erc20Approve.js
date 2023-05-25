5c945bb499397e890624dc4756468355704509f6
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-05-25 12:25:14 +0530

    erc20Approve and usdc out

--- packages/core/test/helpers/erc20Approve.js
+++ packages/core/test/helpers/erc20Approve.js
@@ -1,18 +1,22 @@
+const { default: BigNumber } = require("bignumber.js");
+const { assert } = require("chai");
 const hre = require("hardhat");
 const { getContract } = hre;
 const Token = getContract("ExpandedERC20");
 
-async function approve(tokenAddress, approver, spender, amount) {
+async function erc20Approve(tokenAddress, approver, spender, amount) {
   const token = await Token.at(tokenAddress);
-
-  const tokenDecimals = await token.methods.decimals().call();
-
-  const amountInWei = hre.ethers.utils.parseUnits(amount, tokenDecimals);
-  const allowance = await token.methods.allowance(approver, spender).call();
-
-  if (amountInWei.gt(allowance)) {
-    await token.methods.approve(spender, amountInWei).send({ from: approver });
-  }
+  await token.methods.approve(spender, amount).send({ from: approver });
+  assert(new BigNumber(await token.methods.allowance(approver, spender).call()).gte(new BigNumber(amount)));
 }
 
-module.exports = approve;
+async function getUsdc(tokenAddress, spender) {
+  const usdcTreasury = "0x75C0c372da875a4Fc78E8A37f58618a6D18904e8";
+  await hre.ethers.provider.send("hardhat_impersonateAccount", [usdcTreasury]);
+  const usdcTreasurysigner = await hre.ethers.getSigner(usdcTreasury);
+  const usdc = await hre.ethers.getContractAt(Token.options.jsonInterface, tokenAddress, usdcTreasurysigner);
+  const transferTx = await usdc.transfer(spender, "1000000000");
+  await transferTx.wait();
+}
+
+module.exports = { erc20Approve, getUsdc };
